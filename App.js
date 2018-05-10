bb3498f68d6fc2ea7bfb528442d1da2fb1e7f9ae
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-10 19:09:27 +0200

    feat: add socket.io buttons to create/join/start games in App screen

--- App.js
+++ App.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import SocketIOClient from 'socket.io-client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import io from 'socket.io-client';
+import logger from 'redux-logger';
+import uuid from 'uuid/v4';
 
-import { localBackendIp } from 'react-native-dotenv'
+import { localBackendIp, localBackendPort } from 'react-native-dotenv';
+
+import reducers from './redux/reducers';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     // update localBackendIp in .env to connect to other machine on the LAN
-    this.ip =  localBackendIp || 'localhost';
-    this.socket = SocketIOClient(`http://${this.ip}:3000`);
+    this.ip = localBackendIp || 'localhost';
+    this.port = localBackendPort || 3000;
+    // this.socket = io(`http://${this.ip}:${this.port}`);
+    this.socket = io(`http://localhost:3000`);
   }
 
-  sendSocketRequest = () => {
-    this.socket.emit('newGame', 'Starting new game...');
+  createGameRequest = () => {
+    this.socket.emit('metaChannel', 'createGame', {
+      user: {id: '98kbkoNjVPCLez_3AAAA' /*uuid()*/, name: 'jackTheRipper', avatar: 'blue'}
+    });
+  }
+
+  joinGameRequest = () => {
+    this.socket.emit('metaChannel', 'joinGame', {
+      gameId: '98kbkoNjVPCLez_3AAAA',
+      user: {id: '98kbkoNjVPCLez_3AAAA' /*uuid()*/, name: 'johnTheRipper', avatar: 'red'}
+    });
+  }
+
+  startGameRequest = () => {
+    this.socket.emit('metaChannel', 'startGame', {
+      gameId: '98kbkoNjVPCLez_3AAAA',
+    });
+  }
+
+  componentDidMount() {
+    this.socket.on('metaChannel', res => console.log(res));
+    this.socket.on('gameChannel', res => console.log(res));
+    this.socket.on('error', res => console.log(res));
   }
 
   render() {
+    const store = createStore(reducers);
     return (
-      <View style={styles.container}>
-        <Button title='Send Socket Request' onPress={this.sendSocketRequest}/>
-      </View>
+      <Provider store={createStore(reducers, applyMiddleware(logger))}>
+        <View style={styles.container}>
+          <Button title='Create Game' onPress={this.createGameRequest}/>
+          <Button title='Join Game' onPress={this.joinGameRequest}/>
+          <Button title='Start Game' onPress={this.startGameRequest}/>
+        </View>
+      </Provider>
     );
   }
 }
